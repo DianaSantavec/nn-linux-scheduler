0df8224c6a3f69fe675dd659a003c515dc03a637
Diana Santavec <diana.santavec@gmail.com>
2020-08-15 12:59:10 +0200

    init is working, filesystem exist

--- tests/build.sh
+++ tests/build.sh
@@ -6,35 +6,51 @@ IMAGES=/home/dianas/project/images
 TESTS=/home/dianas/project/nn-linux-scheduler/tests
 DOCKER=/home/dianas/project/docker
 
+BUSYBOXW=/home/dianas/project/busybox-whole
+BUSYBOX=/home/dianas/project/busybox
+
+INIT=$TESTS/init
+
 CNAME="ubuntu_terminal"
 
 docker start $CNAME
 
-if [ "$1" != "-t" ]; then
+if [ "$1" != "-r" ];then
+    if [ "$1" != "-t" ]; then
 
-    #CREATE CONFIGURATION
-    docker exec -w $CUSTOMD $CNAME make defconfig
+        #CREATE CONFIGURATION
+        docker exec -w $CUSTOMD $CNAME make defconfig
 
-    #BUILD KERNEL
-    docker exec -w $CUSTOMD $CNAME make -j8
-    docker exec -w $CUSTOMD $CNAME make modules
+        #BUILD KERNEL
+        docker exec -w $CUSTOMD $CNAME make -j8
+        docker exec -w $CUSTOMD $CNAME make modules
 
-    docker exec -w $CUSTOMBZ $CNAME mv bzImage $IMAGES
+        docker exec -w $CUSTOMBZ $CNAME mv bzImage $IMAGES
 
+    #BUSYBOX 
+    docker exec -w $BUSYBOXW $CNAME make defconfig
+    docker exec -w $BUSYBOXW $CNAME sed -i 's|# CONFIG_STATIC is not set|CONFIG_STATIC=y|g' .config
+    docker exec -w $BUSYBOXW $CNAME make -j8
+    docker exec -w $BUSYBOXW $CNAME make install
+
+    fi
+
+    docker exec -w $BUSYBOX $CNAME rm -r bin sbin etc proc sys usr init dev/null
+    docker exec -w $BUSYBOX $CNAME mkdir -p bin sbin etc proc sys usr/bin usr/sbin dev/null
+    docker exec -w $BUSYBOX $CNAME cp -a $BUSYBOXW/_install/* .
+
+    docker exec -w $BUSYBOX $CNAME cp $INIT .
+    docker exec -w $BUSYBOX $CNAME chmod +x init
+
+    docker exec -w $TESTS $CNAME ./compress-initramfs.sh
+
+
+    docker stop $CNAME
 fi
 
-#BUILD INIT
-docker exec -w $TESTS $CNAME gcc -static init.c -o init
-docker exec -i -w $TESTS $CNAME echo init | cpio -o -H newc | gzip > test.cpio.gz
-
-#MAKE INITRAMFS
-
-#docker exec -w $CUSTOMBZ $CNAME mkinitramfs -o initrd.img-2.6.22
-#docker exec -w $CUSTOMBZ $CNAME mv initrd.img-2.6.22 $IMAGES
-
-docker stop $CNAME
-
-mv $TESTS/test.cpio.gz $IMAGES
-
 cd $IMAGES
-qemu-system-x86_64 -kernel bzImage -nographic -append "console=ttyS0" -initrd test.cpio.gz -m 512
+
+#QEMU
+qemu-system-x86_64 -kernel bzImage \
+  -initrd initramfs.cpio.gz -nographic \
+  -append "console=ttyS0"
