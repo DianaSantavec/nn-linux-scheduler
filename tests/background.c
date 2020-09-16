e25cb54bc24d1cf8c62db41c60ee8d49fcb9770e
Diana Santavec <diana.santavec@gmail.com>
2020-09-16 15:54:51 +0200

    cpu testing for hashing is done

--- tests/background.c
+++ tests/background.c
@@ -3,9 +3,11 @@
 int main(int argc, char *argv[]){
     unsigned int i,j;
     unsigned int sum = 0;
-    for (j=0;j<4294967295;j++){
-            sum+=1;
+    for (i=0;i<4294967295;i++){
+        for (j=0;j<4294967295;j++){
+                sum+=1;
+        }
+        sum = 0;
     }
-    printf("%s is done\n", argv[1]);
 return 0;
 }
