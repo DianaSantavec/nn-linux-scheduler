03c7e9dadcf5c3da2f62bdc9864482fe72201098
Diana Santavec <diana.santavec@gmail.com>
2020-09-20 13:12:57 +0200

    branch files

--- tests/test.c
+++ tests/test.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]){
     unsigned int i,j;
     unsigned int sum = 0;
-    for (j=0;j<4294967295;j++){
+    for (j=0;j<2100000000;j++){ //
             sum+=1;
     }
     printf("%s is done\n", argv[1]);
