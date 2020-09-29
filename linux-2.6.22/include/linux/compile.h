8691dd20f97bf60c1296ff7c885acc16fa94de7d
Diana Santavec <diana.santavec@gmail.com>
2020-09-30 00:48:35 +0200

    test for cpu ready, init changed

--- linux-2.6.22/include/linux/compile.h
+++ linux-2.6.22/include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#6 SMP Sun Sep 27 18:48:36 UTC 2020"
-#define LINUX_COMPILE_TIME "18:48:36"
+#define UTS_VERSION "#7 SMP Tue Sep 29 17:26:09 UTC 2020"
+#define LINUX_COMPILE_TIME "17:26:09"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "096a804d3837"
 #define LINUX_COMPILE_DOMAIN ""
